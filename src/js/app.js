ea57776a05f034fa5825037e1bdc8764d1aea183
Shashanka Nataraj <shashankan.10@gmail.com>
2014-07-29 21:48:04 +0530

    Added mechanism to dynamically construct menu
    The menu is now read from menus.json, and is shown as a a series of
    bootstrap dropdowns!

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-console.log($);
+var eyeApp = angular.module('eyeApp', []);
+
+eyeApp.controller('eyeMenuCtrl', ['$scope','$http',function ($scope,$http) {
+  $http.get('menu.json').success(function(data) {
+    $scope.menus = data;
+  });
+}]);
