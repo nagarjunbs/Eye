cc596d30b0ef23bbe3eb49db898c9372d3d31c25
Shashanka Nataraj <shashankan.10@gmail.com>
2014-08-05 19:49:40 +0530

    Relocated the event handler for file open
    Relocated the file open event handler to the controller instead of the
    service.

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -56,8 +56,37 @@ eyeApp.controller('eyeMenuController', ['$scope','$http','$rootScope','FileSyste
   
   
   // Register event handlers for services
+  //Handler for the new file event
   $rootScope.$on('file-new',fsService.newFile);
-  $rootScope.$on('file-open',$.proxy(fsService.openFile,$scope));
+  //Handler for the file open event
+  $rootScope.$on('file-open',$.proxy(function(){
+    // Preserve the scope passed to this function via jquery proxies throughout this chain since chrome switches to global scope given no scope
+      var chooseFileEntryCallback = $.proxy(function(fileObject){
+          //Check if the user pressed the cancel button, in which case, there would be no fileObject
+          if (fileObject){
+            // File available callback
+            var callback = $.proxy(function(fileEntry,chosenFileObject) {
+              // Make a new file reader
+              var reader = new FileReader();
+              //Create a proxy with an injected scope
+              var readerProxy = $.proxy(function(fileEntry,chosenFileObject,event){
+                //Emit an event after reading is done
+                this.$emit('load-opened-file-content',fileEntry.name,event.target.result);
+              },this,fileEntry,chosenFileObject);
+              // Attach the scoped event handler
+              reader.onloadend = readerProxy;
+              // Trigger the read operation, which will call the handler above after it finishes
+              reader.readAsText(chosenFileObject);
+            },this,fileObject)
+            fileObject.file(callback);
+          }
+      },this);
+      
+      // Trigger the file browser dialog in chrome
+      chrome.fileSystem.chooseEntry({
+          type: 'openFile'
+      },chooseFileEntryCallback);
+  }));
   $rootScope.$on('file-openfolder',fsService.openFolder);
   $rootScope.$on('file-savefile',fsService.saveFile);
   $rootScope.$on('file-savefileas',fsService.saveFileAs);
